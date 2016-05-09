b0fb14b234e89b5481de94d4df8ba8ec81480698
Derrick Brown <derrick.brown@w-dn-ml-dbrown.local>
2016-05-09 09:33:55 -0600

    ract-redux connect

--- test/voting.spec.js
+++ test/voting.spec.js
@@ -7,7 +7,7 @@ import {
 } from 'react-addons-test-utils';
 import { List } from  'immutable';
 import Voting from '../src/components/Voting';
-import {expect} from 'chai';
+import { expect } from 'chai';
 
 
 describe('Voting', () => {
